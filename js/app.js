c1d138fd2c4b2bb06a2e983e6c04db53f1ba0e64
Felipe Costa <speratifelipe1@gmail.com>
2024-10-02 00:51:56 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function adicionar () {
+
+    let produtoCompleto = document.getElementById('produto').value;
+    let nomeProduto = produtoCompleto.match(/^\S+\s*/)[0];
+    let valorProduto = produtoCompleto.match(/R\$(\d+)/)[1];
+    let valorProdutoRS = produtoCompleto.match(/R\$(\d+)/)[0];
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let valor = valorProduto * quantidade
+
+    console.log(`${valorProduto} x ${quantidade} = ${valor}`);
+
+    let listaDeProdutos = document.querySelector('.carrinho__produtos__produto').textContent;
+    adicionarNaLista (quantidade, nomeProduto, valorProdutoRS);
+
+    somarNoTotal (quantidade, valorProduto);
+
+}
+
+function adicionarNaLista (qtd, nome, valor) {
+    let listaDeProdutosHTML = document.querySelector('.carrinho__produtos__produto');
+    let qtdX = `${qtd}x`
+    listaDeProdutosHTML.innerHTML += `<br><span class="texto-azul">${qtdX}</span> ${nome} <span class="texto-azul">${valor}</span>`
+}
+
+function somarNoTotal (qtd, valor) {
+    let valorTotalBruto = document.querySelector('.carrinho__total');
+    let valorTotalInicial = parseInt(valorTotalBruto.textContent.match(/\d+/)[0]);
+    let valorTotalFinal = valorTotalInicial + (qtd * valor);
+    valorTotalBruto.innerHTML = `Total: <span class="texto-azul" id="valor-total">R$${valorTotalFinal}</span>`;
+    console.log(valorTotalBruto);
+}
